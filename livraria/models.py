bc71d6ea800787fbaf5f3569c6c5a9796d09e502
isabellirosa <isabelli.ifc@gmail.com>
2025-02-18 15:34:26 -0300

    FEAT: adicionado livro pela API e arrumando detalhes na model

--- livraria/models.py
+++ livraria/models.py
@@ -12,8 +12,14 @@ class Aluno(models.Model):
     matricula = models.IntegerField()
     curso = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
 class Emprestimos(models.Model):
     aluno = models.ForeignKey(Aluno, on_delete=models.PROTECT)
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT)
     data_emprestimo = models.DateField()
     data_devolucao = models.DateField()
+
+    def __str__(self):
+        return self.livro.titulo
